8bccaa7646772000da09c70252ce1a8109586a5f
soenneker <jake@soenneker.com>
2026-09-11 19:18:02 -0500

    cleanup

---
+++ test/Soenneker.Blazor.Floating.Windows.Demo/Pages/DataItem.cs
@@ -0,0 +1,9 @@
+namespace Soenneker.Blazor.Floating.Windows.Demo.Pages;
+
+internal class DataItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Status { get; set; } = "";
+    public int Progress { get; set; }
+}
